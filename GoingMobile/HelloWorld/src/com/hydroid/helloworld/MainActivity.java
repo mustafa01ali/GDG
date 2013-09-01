4ece3cf7f388efaf7affd47cacdf003c57230022
Mustafa Ali <mustafa01ali@gmail.com>
2013-09-01 15:00:26 +0530

    Implemented main screen

--- GoingMobile/HelloWorld/src/com/hydroid/helloworld/MainActivity.java
+++ GoingMobile/HelloWorld/src/com/hydroid/helloworld/MainActivity.java
@@ -1,19 +1,62 @@
 package com.hydroid.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	private final String LOG_TAG = "HYDROID";
+	
+	private EditText nameET;
+	private EditText emailET;
+	
+	private Button submitBtn;
+	private Button clearBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initUI();
     }
 
+    private void initUI() {
+    	nameET = (EditText) findViewById(R.id.editText1);
+    	emailET = (EditText) findViewById(R.id.editText2);
+    	submitBtn = (Button) findViewById(R.id.button1);
+    	clearBtn = (Button) findViewById(R.id.button2);
+    	
+    	submitBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String name = nameET.getText().toString();
+				String email = emailET.getText().toString();
+				
+				Log.d(LOG_TAG, "Name: " + name);
+				Log.i(LOG_TAG, "Email: " + email);
+				
+			}
+		});
+    	
+    	clearBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				nameET.setText("");
+				emailET.setText("");
+				nameET.requestFocus();
+			}
+		});
+	}
 
-    @Override
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
